2b167f80aa8629b1ccc6a4e098cf4e867d679cb7
Dan Rovito <danrovito@gmail.com>
2019-11-30 12:58:34 -0500

    initial

---
+++ src/CommanderServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Darovi\Commander;
+use Illuminate\Support\ServiceProvider;
+
+class CommanderServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+    }
+}
